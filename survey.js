ef0b499bd56c0cdd95441a398f1cf107e82eda4e
mhakbar <habibakbar@live.com>
2022-01-23 15:03:40 -0800

    callback hell

--- survey.js
+++ survey.js
@@ -5,48 +5,27 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-let questions = {
-  0:"What's your name? Nicknames are also acceptable :)",
-  1:"What's an activity you like doing?",
-  2:"What do you listen to while doing that?",
-  3:"Which meal is your favourite (eg: dinner, brunch, etc.)",
-  4:"What's your favourite thing to eat for that meal?",
-  5:"Which sport is your absolute favourite?",
-  6:"What is your superpower? In a few words, tell us what you are amazing at!"
+for (let i = 0; i < 7; i++) {
+rl.question('What do you think of Node.js? ', (answer0) => {
+  rl.question('What\'s an activity you like doing?', (answer1) => {
+    rl.question('What do you listen to while doing that?', (answer2) => {
+      rl.question('Which meal is your favourite (eg: dinner, brunch, etc.)', (answer3) => {
+        rl.question('What\'s your favourite thing to eat for that meal?', (answer4) => {
+          rl.question('Which sport is your absolute favourite?', (answer5) => {
+            rl.question('What is your superpower? In a few words, tell us what you are amazing at!', (answer6) => {
+
+            
+              console.log(`Your thoughts: ${answer0}. Activity you like: ${answer1}. What you listen to: ${answer2}. Favourite meal: ${answer3}. What do you eat during this meal: ${answer4}. Favourite sports: ${answer5}. Your superpowers are: ${answer6}. `);  
+            
+
+  rl.close();
+});
+});
+});
+});
+});
+});
+});
 };
-
-let answers = {
-  0: "",
-  1: "",
-  2: "",
-  3: "",
-  4: "",
-  5: "",
-  6: ""
-};
-
-let i = 0;
-
-const question = function () {
-  if (i < 7) {
-    rl.question(`${questions[i]}\n`, (answer) => {
-      answers[i] = answer;
-      i++;
-      console.log()
-      question()
-    });
-  } else {
-    rl.close()
-    setTimeout(() => console.log(`
-    ${answers[0]} likes ${answers[1]} while listening to ${answers[2]}. 
-    They like eating ${answers[4]} for their favorite meal ${answers[3]}. 
-    ${answers[4]}'s absolute favorite sport is ${answers[5]}.
-    Your superpowers are: ${answers[6]}
-  `), 1000);
-  }
-}
-
-question()
-
 
 
